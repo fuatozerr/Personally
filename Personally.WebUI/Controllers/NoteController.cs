55b86cd4e663a478eb54b00e2540f93b839d4fbd
fuat özer <fuatozerr23@gmail.com>
2019-09-25 22:59:12 +0300

    notların detay sayfası olusturuldu ve örnek resimlerle yüklendi

--- Personally.WebUI/Controllers/NoteController.cs
+++ Personally.WebUI/Controllers/NoteController.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Personally.Business.Abstract;
+using Personally.Entities;
 
 namespace Personally.WebUI.Controllers
 {
     public class NoteController : Controller
     {
+        private INoteService _noteService;
+        public NoteController(INoteService noteService)
+        {
+            _noteService = noteService;
+
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+
+            Note noteDetail = _noteService.GetById((int)id);
+            return View(noteDetail);
         }
     }
 }
